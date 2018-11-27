f62a2889973fbf8f62ca8c876ac0017a2d810b06
Matt Coley <mcoley@hubspot.com>
2018-11-27 15:20:54 -0500

    Fix timeout leading to queue deadlock.

--- src/main/java/net/spy/memcached/protocol/TCPMemcachedNodeImpl.java
+++ src/main/java/net/spy/memcached/protocol/TCPMemcachedNodeImpl.java
@@ -296,7 +296,7 @@ public abstract class TCPMemcachedNodeImpl extends SpyObject implements
           getLogger().debug("Not writing cancelled op.");
           Operation cancelledOp = removeCurrentWriteOp();
           assert o == cancelledOp;
-        } else if (o.isTimedOut(defaultOpTimeout)) {
+        } else if (o.isTimedOut() || o.isTimedOut(defaultOpTimeout)) {
           getLogger().debug("Not writing timed out op.");
           Operation timedOutOp = removeCurrentWriteOp();
           assert o == timedOutOp;
