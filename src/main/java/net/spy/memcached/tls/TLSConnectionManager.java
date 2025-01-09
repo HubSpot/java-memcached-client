7b91f4aeb2c2d3365d42ecf8c96484931fca5678
Sean Levorse <slevorse@hubspot.com>
2025-01-09 12:51:27 -0500

    Trying without closing channels after handshake

--- src/main/java/net/spy/memcached/tls/TLSConnectionManager.java
+++ src/main/java/net/spy/memcached/tls/TLSConnectionManager.java
@@ -106,12 +106,12 @@ public class TLSConnectionManager implements Closeable {
                                 return false;
                             }
                             // We're done with the handshake, signal that we aren't going to be sending or receiving any more data
-                            try {
-                                sslEngine.closeInbound();
-                            } catch (SSLException e) {
-                                LOG.warn("{} - tried to close inbound traffic but has not received a TLS close notification yet due to end of stream.", socketChannel.getRemoteAddress(), e);
-                            }
-                            sslEngine.closeOutbound();
+//                            try {
+//                                sslEngine.closeInbound();
+//                            } catch (SSLException e) {
+//                                LOG.warn("{} - tried to close inbound traffic but has not received a TLS close notification yet due to end of stream.", socketChannel.getRemoteAddress(), e);
+//                            }
+//                            sslEngine.closeOutbound();
                             break;
                         }
                         networkInBuffer.flip();
