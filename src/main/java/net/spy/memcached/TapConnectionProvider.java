6f8be931b213642c3e1a5980ce363ba8380df7e5
Sean Levorse <slevorse@hubspot.com>
2025-08-15 16:22:37 -0400

    Undoing autoformatting

--- src/main/java/net/spy/memcached/TapConnectionProvider.java
+++ src/main/java/net/spy/memcached/TapConnectionProvider.java
@@ -30,6 +30,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+
 import net.spy.memcached.auth.AuthDescriptor;
 import net.spy.memcached.auth.AuthThreadMonitor;
 import net.spy.memcached.compat.SpyObject;
